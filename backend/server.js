8230d18a61a43eb52703d851a8c65c8bf831c850
dbobzin <dbobzin@ilstu.edu>
2023-05-04 17:42:59 -0500

    add path

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
 
+const path = require("path");
+
 // express app
 const app = express();
 
